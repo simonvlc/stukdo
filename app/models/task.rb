b22c9751efe5b9ded799e74e6778439094e2f65e
Simón Muñoz <simonvlc@gmail.com>
2016-08-20 12:30:16 +0200

    Added auto_html

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :user
+  auto_html_for :content do
+    html_escape
+    image
+    youtube(width: '100%', height: 250 )
+    link target: '_blank', rel: 'nofollow'
+    simple_format
+  end
 end
